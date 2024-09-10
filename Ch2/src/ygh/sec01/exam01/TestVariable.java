ccf04c8f2d76d6b02b50d501d3087a6fc0068afc
Geonho Yoo <ygh7820@s.scnu.ac.kr>
2024-09-10 10:49:04 +0900

    Ch2 update

---
+++ Ch2/src/ygh/sec01/exam01/TestVariable.java
@@ -0,0 +1,12 @@
+package ygh.sec01.exam01;
+
+public class TestVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 0;
+		
+		System.out.println(""+a);
+	}
+
+}
